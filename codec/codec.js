f9b406e5be6fe95c24bb7ae34f8a5a2e85358d1b
Fenn Saji <fenn@swnglobal.com>
2023-11-16 12:06:32 +0530

    Codec [In Progress]

---
+++ codec/codec.js
@@ -0,0 +1,50 @@
+const { TypeRegistry, createType } = polkadotTypes;
+
+let rawBytes = document.getElementById('rawBytes');
+let customTypes = document.getElementById('customTypes');
+let output = document.getElementById('output');
+
+customTypes.addEventListener('input', parseCustomType);
+rawBytes.addEventListener('input', parseCustomType);
+
+/* CUSTOM TYPES EDITOR START */
+
+// create the editor
+const options = { mode: 'code', onChange: parseCustomType };
+const editor = new JSONEditor(customTypes, options);
+
+// set json
+const initialJson = [{ MyVec: 'Vec<u16>' }];
+editor.set(initialJson);
+
+/* CUSTOM TYPES EDITOR END */
+
+const registry = new TypeRegistry();
+
+function parseCustomType() {
+  try {
+    let typesObject = editor.get();
+
+    let lastTypeKey;
+
+    if (Array.isArray(typesObject)) {
+      typesObject.map(type => {
+        registry.register(type);
+      });
+
+      let lastTypeObject = typesObject[typesObject.length - 1];
+      lastTypeKey = Object.keys(lastTypeObject)[0];
+    } else {
+      registry.register(typesObject);
+      lastTypeKey = Object.keys(typesObject)[0];
+    }
+
+    output.innerText = JSON.stringify(
+      createType(registry, lastTypeKey, rawBytes.value.trim())
+    );
+  } catch (e) {
+    output.innerText = e;
+  }
+}
+
+parseCustomType();
